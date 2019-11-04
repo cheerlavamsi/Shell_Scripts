9af6f8678f980d1e069cd2fdd341a0bdbc82ca9f
chvamsidevops <chvamsi.devops@gmail.com>
2019-11-04 22:28:41 +0530

    Adding Application Service

--- MonolithicServices/StudentReg.sh
+++ MonolithicServices/StudentReg.sh
@@ -10,3 +10,20 @@ curl https://studentapi-cit.s3-us-west-2.amazonaws.com/studentapp-frontend.tar.g
 systemctl enable nginx
 
 systemctl start nginx
+
+yum install java -y
+
+useradd student
+
+su - student
+
+curl http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.47/bin/apache-tomcat-8.5.47.tar.gz | tar -xz
+
+cd apache-tomcat-8.5.47
+
+wget https://s3-us-west-2.amazonaws.com/studentapi-cit/student.war -O webapps/student.war
+
+wget https://s3-us-west-2.amazonaws.com/studentapi-cit/mysql-connector.jar -O lib/mysql-connector.jar
+
+bin/startup.sh
+
