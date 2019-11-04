d5656a87e9520ac0280dfe4b0d1ecfe32427604b
chvamsidevops <chvamsi.devops@gmail.com>
2019-11-04 22:11:26 +0530

    Starting the WebService

--- MonolithicServices/StudentReg.sh
+++ MonolithicServices/StudentReg.sh
@@ -1,1 +1,12 @@
 #!bin/bash
+yum install nginx -y
+
+cd /usr/share/nginx/html
+
+rm -rf *
+
+curl https://studentapi-cit.s3-us-west-2.amazonaws.com/studentapp-frontend.tar.gz | tar -xz 
+
+systemctl enable nginx
+
+systemctl start nginx
