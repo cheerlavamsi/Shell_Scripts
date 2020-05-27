2ccf5a52de17b3bcf7b581cfc7b3e8daadff931d
cheerlavamsi <chvamsi.devops@gmail.com>
2020-05-27 17:50:10 +0530

    new Swiss re code

---
+++ Swiss Re/ReplacePropertyfile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# repos=( 
+#   "/c/projects/project-1"
+#   "/c/projects/project-2"
+#   "/c/projects/project-3"
+# )
+
+repos=( 
+  "https://github.com/cheerlavamsi/ansible.git"
+)
+
+echo ""
+echo "Getting latest for" ${#repos[@]} "repositories using pull --rebase"
+
+for repo in "${repos[@]}"
+do
+  echo ""
+  echo "****** Getting latest for" ${repo} "******"
+  cd "${repo}"
+  git pull --rebase
+  echo "******************************************"
+done
